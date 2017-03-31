578fd1ac4da947532fd08e24696dd4a200ae6ff6
wangyang <18518317472@163.com>
2017-03-31 17:32:38 +0800

    0

--- DatePicker.Web/wwwroot/js/side.js
+++ DatePicker.Web/wwwroot/js/side.js
@@ -3,10 +3,9 @@
     var right_con = $(".right");
     $(".sub_left li").click(function () {
         var item = $(this);
-
         var html_url = item.attr("html");
         var params = item.attr("params");
-        console.log(params);
+        window.history.replaceState({},"", params);
         right_con.loading();
         $.get(html_url, function (data) {
             right_con.loading();
@@ -19,5 +18,15 @@
             datepicker();
         });
     });
-    current.click();
+    var tag = getQueryString("tag");
+    if(tag){
+      $("#"+tag).click();
+    }else{
+      current.click();
+    }
 });
+function getQueryString(name) { 
+   var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+   var r = window.location.search.substr(1).match(reg); 
+   if (r != null) return unescape(r[2]); return null; 
+}
